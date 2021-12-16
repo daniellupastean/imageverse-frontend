40669658d1dc8915c05d39ec786c3f5cfd04e7a6
Daniel Lupastean <lupasteandaniell1@gmail.com>
2021-12-16 15:29:57 +0200

    Start refactoring all code & fix sidebar

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,20 +1,40 @@
-import {Form,Title,Button,Input,TxtP,TxtSpan,Container} from "../components/form.styled"
-import { Link, useNavigate } from "react-router-dom";
+import {
+  Form,
+  Title,
+  Button,
+  Input,
+  Motto,
+  TxtSpan,
+  Container,
+} from '../components/form.styled';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../components/Logo';
 
 export default function LoginPage() {
   let navigate = useNavigate();
   return (
     <Container>
-    <div><TxtSpan style={{"color": "#359AEF"}}>IMAGE</TxtSpan><TxtSpan style={{"color": "#FFFFFF"}}>VERSE</TxtSpan></div>
-    <TxtP>A place for your pictures</TxtP>
-  <Form>
-    <Title>Login to continue</Title>
-    <Input type={"text"} placeholder='Email' required/>
-    <Input type={"password"} placeholder='Password' required/>
-    <p style={{"width":"75%","text-align":"right","margin-top":"5px"}}>Forgot your <Link to="/reset-password">password</Link> ?</p>
-    {/*Aici ar fi trebuit sa fie un input type="submit"*/}
-    <Button onClick={() => {navigate("/home")}}>LOGIN</Button>
-    <p>Don't have an account? <Link to="/register">Register</Link></p>
-  </Form>
-  </Container>); 
-    }
+      <Logo />
+      <Motto>A place for your pictures</Motto>
+      <Form autoComplete="off">
+        <Title>Login to continue</Title>
+        <Input type={'text'} placeholder="Email" required />
+        <Input type={'password'} placeholder="Password" required />
+        <p style={{ width: '75%', 'text-align': 'right', 'margin-top': '5px' }}>
+          Forgot your <Link to="/reset-password">password</Link> ?
+        </p>
+        {/*Aici ar fi trebuit sa fie un input type="submit"*/}
+        <Button
+          onClick={() => {
+            navigate('/home');
+          }}
+        >
+          LOGIN
+        </Button>
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </Form>
+    </Container>
+  );
+}
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,20 +1,37 @@
-import {Form,Title,Button,Input,TxtSpan,TxtP,Container} from "../components/form.styled"
-import { Link, useNavigate } from "react-router-dom";
+import {
+  Form,
+  Title,
+  Button,
+  Input,
+  TxtSpan,
+  Motto,
+  Container,
+} from '../components/form.styled';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../components/Logo';
 
-
-export default function LoginPage() {
-    let navigate = useNavigate();
+export default function RegisterPage() {
+  let navigate = useNavigate();
   return (
     <Container>
-    <div><TxtSpan style={{"color": "#359AEF"}}>IMAGE</TxtSpan><TxtSpan style={{"color": "#FFFFFF"}}>VERSE</TxtSpan></div>
-    <TxtP>A place for your pictures</TxtP>
-  <Form>
-    <Title>Create an account</Title>
-    <Input type={"text"} placeholder='Email'/>
-    <Input type={"password"} placeholder='Password'/>
-    <Input type={"password"} placeholder='Confirm password'/>
-    <Button onClick={() => {navigate("/login")}}>REGISTER</Button>
-    <p>Already have an account? <Link to="/login">Login</Link></p>
-  </Form>
-  </Container>);
-    }
+      <Logo />
+      <Motto>A place for your pictures</Motto>
+      <Form autoComplete="off">
+        <Title>Create an account</Title>
+        <Input type={'text'} placeholder="Email" />
+        <Input type={'password'} placeholder="Password" />
+        <Input type={'password'} placeholder="Confirm password" />
+        <Button
+          onClick={() => {
+            navigate('/login');
+          }}
+        >
+          REGISTER
+        </Button>
+        <p>
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </Form>
+    </Container>
+  );
+}
